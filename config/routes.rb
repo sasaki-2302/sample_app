ff1c67932c5478356b125ec1f0b0ad81f60d6719
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-02-17 05:05:23 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'user#create'
   resources :users
   root 'static_pages#home'
 end
