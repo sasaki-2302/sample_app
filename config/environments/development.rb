152a58cb1631b5bb2b6d00f0bb77a41b3d2982cb
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-02-19 09:29:22 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'ae4101ce52a64046a0f728bc3f818879.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
