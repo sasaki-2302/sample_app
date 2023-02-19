ad33986a694cda31ab1b9de313755d540d396633
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-02-19 07:59:18 +0000

    create_user_mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
+User.create!(name:  "xxx",
+             email: "x@x.com",
+             password:              "xxxxxx",
+             password_confirmation: "xxxxxx",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             activated: true,
+             activated_at: Time.zone.now)
 
+User.create!(name:  "yyy",
+             email: "y@y.com",
+             password:              "yyyyyy",
+             password_confirmation: "yyyyyy",
+             activated: true,
+             activated_at: Time.zone.now)
+
+User.create!(name:  "zzz",
+             email: "z@z.com",
+             password:              "zzzzzz",
+             password_confirmation: "zzzzzz",
+             activated: true,
+             activated_at: Time.zone.now)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
