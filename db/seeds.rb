45a199710080486f57d4a753c1ec0244ddcf3b8f
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-02-21 06:32:52 +0000

    Add_user_microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,28 @@ User.create!(name:  "xxx",
              activated: true,
              activated_at: Time.zone.now)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             activated: true,
-             activated_at: Time.zone.now)
-
-User.create!(name:  "yyy",
-             email: "y@y.com",
-             password:              "yyyyyy",
-             password_confirmation: "yyyyyy",
-             activated: true,
-             activated_at: Time.zone.now)
-
 User.create!(name:  "zzz",
              email: "z@z.com",
              password:              "zzzzzz",
              password_confirmation: "zzzzzz",
              activated: true,
              activated_at: Time.zone.now)
+
+20.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.paragraph
+  users.each { |user| user.microposts.create!(content: content) }
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
